e337bcde1cddb5b29932a8b77455862b3d064f60
heeyoung00 <ils09019@naver.com>
2025-09-02 14:17:31 +0900

    프로필수정 모달 컴포넌트화

--- src/mypage/Mypage.jsx
+++ src/mypage/Mypage.jsx
@@ -10,6 +10,7 @@ import editcourse from '/icons/editCourse.png';
 import edit from '/icons/edit.png';
 import coursedelete from '/icons/delete.png';
 import Modal from "react-modal";
+import ProfileEditModal from "../components/profileModal"; 
 
 Modal.setAppElement('#root');
 
@@ -146,35 +147,10 @@ export default function Mypage() {
                 </div>
             </div>
 
-            <Modal
-                isOpen={isProfileModalOpen}
-                onRequestClose={() => setIsProfileModalOpen(false)}
-                className="ProfileEdit-modal"
-                style={{
-                    overlay: { backgroundColor: "rgba(0,0,0,0.5)" },
-                }}
-            >
-                <div className='modal-profile-edit-top'>
-                    프로필 수정
-                    <span>프로필 사진</span>
-                </div>
-                <div className='modal-profile-img-edit'>
-                    <img src={profile} alt="프로필 사진" />
-                    <div className='modal-profile-img-buttons'>
-                        <button>사진 변경</button>
-                        <button>삭제</button>
-                    </div>
-                </div>
-                <hr style={{ border: "2px solid #E7ECF1"}} />
-                <div className='modal-nickname-box'>
-                    <span>닉네임</span>
-                    <input type="text" placeholder='김촌스' />
-                </div>
-                <div className='modal-edit-finish-buttons'>
-                    <button onClick={() => setIsProfileModalOpen(false)}>취소</button>
-                    <button onClick={() => setIsProfileModalOpen(false)}>적용</button>
-                </div>
-            </Modal>
+            <ProfileEditModal 
+                isOpen={isProfileModalOpen} 
+                onClose={() => setIsProfileModalOpen(false)} 
+            />
             <Modal
                 isOpen={isDelAccountModalOpen}
                 onRequestClose={handleDelCancel}
